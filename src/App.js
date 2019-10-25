1e49f79a6e1c9e573e3c1f42dd407e7030f4db61
Student <example@example.com>
2019-10-25 10:58:06 -0700

    add chaging image on mouse hover and click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Puppy from './puppy'
+import './App.css'
+import BlueBox from './img/blue.jpg'
+import RedBox from './img/red.jpg'
+import GreenBox from './img/green.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      img: BlueBox
+    }
+    //function binds
+    this.redbox = this.redbox.bind(this)
+    this.bluebox = this.bluebox.bind(this)
+    this.greenbox = this.greenbox.bind(this)
+  }
+
+  redbox() {
+    this.setState({ img: RedBox })
+  }
+
+  bluebox() {
+    this.setState({ img: BlueBox })
+  }
+
+  greenbox() {
+    this.setState({ img: GreenBox })
+  }
+
+  render() {
+    return(
+      <div className="App" onMouseDown={this.greenbox} onMouseUp={this.redbox} onMouseEnter={this.redbox} onMouseLeave={this.bluebox}>
+        <header className="App-header">
+          Hello World
+        <img src={this.state.img} />
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/puppy.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Puppy extends Component {
+  constructor() {
+    super()
+    this.state = {
+      //states
+    }
+    //function binds
+  }
+
+  render() {
+
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Puppy;
